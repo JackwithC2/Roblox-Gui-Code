d2ef47069226a89d7383b27501c96db13ed5324b
Hold12Rl <86273091+Hold12Rl@users.noreply.github.com>
2021-06-21 22:51:49 -0700

    manifest updater!!!!!

---
+++ Github/RobloxStuff/manifest.lua
@@ -0,0 +1,11 @@
+-- ok This is gonna take some time
+
+Verison_(1.0)
+GitVerison_(1.2)
+Updater_ (1.0)
+
+Locations{
+verison:ProjectRealse
+GitVerison:https://Roblox-Gui-Code
+Updater:Github.games.https://github.com/Hold12Rl/Roblox-Gui-Code/blob/main/Github/games/Project%20updater
+}
